45fe811ab5143b0a9658c79fa1444771bb26930d
Patryk Baryła <44240730+wranidlo@users.noreply.github.com>
2021-03-03 12:44:56 +0100

    Created project

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # library
+
 RESTful service
---
+++ src/test/java/com/baryla/library_service/LibraryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baryla.library_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
